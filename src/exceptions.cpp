a75597d0f6f95a544763d7e55991df3b163dab99
lalaalal <100madalala@gmail.com>
2019-06-18 22:21:51 +0900

    Exception

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -9,3 +9,9 @@ Exception::Exception(int err) : errCode(err) { }
 void Exception::print() const {
     printf("[%02x] ERR\n", errCode);
 }
+
+SocketException::SocketException(int err) : Exception(SOCKET_EXCEPTION | err) { }
+
+void SocketException::print() const {
+    printf("[%02x] SOCKET ERR\n", errCode);
+}
--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -5,7 +5,23 @@
 
 namespace umbrella {
     enum GENARAL_EXCEPTION {
-        WRONG_ARGS  = 0x00,
+        WRONG_ARGS   = 0x00,
+        WRONG_RESULT = 0x01,
+        NOT_INITIALIZED =  0x02
+    };
+
+    enum SOCKET_EXCEPTION {
+        SOCKET_EXCEPTION = 0x10,
+
+        CREATE  = 0x00,
+        BIND    = 0x01,
+        CONNECT = 0x02,
+
+        WRITE   = 0x03,
+        READ    = 0x04,
+
+        WRONG_DATA    = 0x05,
+        NO_CONNECTION = 0x06,
     };
 
     class Exception {
@@ -14,6 +30,12 @@ namespace umbrella {
     public:
         Exception(int err);
         virtual void print() const;
+    };
+
+    class SocketException : public Exception {
+    public:
+        SocketException(int err);
+        void print() const override;
     };
 }
 
