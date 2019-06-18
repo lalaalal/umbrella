954c39b83f190f18761a00389c888af63fedb847
lalaalal <100madalala@gmail.com>
2019-06-18 22:26:29 +0900

    Prepare main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include "umbrella_handler.hpp"
+#include "exceptions.hpp"
 
+using namespace umbrella;
 
-int main(int argc, char const * argv[]) {
+int main(int argc, char * argv[]) {
+    try {
+        UmbrellaHandler umbrellaHandler = UmbrellaHandler();
+        
+    } catch (Exception & e) {
+        e.print();
+    } catch (sql::SQLException & e) {
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+    }
 
     return 0;
 }
